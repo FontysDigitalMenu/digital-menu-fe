b4e5977dac89c10b08aa27fe68e20244d121904f
Hu,Tony T.C. <tony.hu@student.fontys.nl>
2024-03-25 11:30:39 +0100

    added router to the develop

--- src/components/admin/tables/TablesCreate.jsx
+++ src/components/admin/tables/TablesCreate.jsx
@@ -1,13 +1,12 @@
-import React, {useContext, useState} from 'react';
+import {useContext, useState} from 'react';
 import {useNavigate} from "react-router-dom";
 import ButtonSubmit from "../../elements/ButtonSubmit.jsx";
 import ToastNotification from "../../notifications/ToastNotification.jsx";
 import ButtonCancel from "../../elements/ButtonCancel.jsx";
 import ConfigContext from "../../../provider/ConfigProvider.jsx";
 import AuthService from "../../../services/AuthService.jsx";
-import SideNav from "../../navigation/SideNav.jsx";
 
-function TablesCreate({setIsAuthenticated}) {
+function TablesCreate() {
     const navigate = useNavigate();
     const config = useContext(ConfigContext);
     const [tableForm, setTableForm] = useState({
@@ -46,8 +45,6 @@ function TablesCreate({setIsAuthenticated}) {
 
     return (
         <>
-            <SideNav setIsAuthenticated={setIsAuthenticated} />
-
             <div className="p-4 sm:ml-64">
                 <form onSubmit={submitTable} className={"flex flex-col gap-y-2"}>
                     <div>
