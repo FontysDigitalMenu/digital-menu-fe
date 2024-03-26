4e208b1d61e1bf1ac60666c5ce698294f294121d
Allertvdw <allertvdw@hotmail.com>
2024-03-26 15:47:46 +0100

    finished confirm order/progress page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import './App.css'
-import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./components/Home.jsx";
 import Login from "./components/authentication/Login.jsx";
-import {useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import Dashboard from "./components/admin/Dashboard.jsx";
 import AuthService from "./services/AuthService.jsx";
 import Tables from "./components/admin/tables/Tables";
@@ -10,71 +10,85 @@ import ScannedTable from "./components/tables/ScannedTable";
 import ConfigContext from "./provider/ConfigProvider.jsx";
 import TablesCreate from "./components/admin/tables/TablesCreate.jsx";
 import TablesEdit from "./components/admin/tables/TablesEdit.jsx";
-import {ToastContainer} from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import CartOverview from "./components/cart/CartOverview.jsx";
-import { v4 } from 'uuid';
+import { v4 } from "uuid";
 import AdminRoot from "./components/AdminRoot.jsx";
 import Root from "./components/Root.jsx";
-import OrderProgress from "./components/order/OrderProgress.jsx"
+import OrderProgress from "./components/order/OrderProgress.jsx";
 
 function App() {
-    const config = useContext(ConfigContext);
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const config = useContext(ConfigContext);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    useEffect(() => {
-        if (!config) return;
+  useEffect(() => {
+    if (!config) return;
 
-        if (config.DEVICE_ID !== null) {
-            localStorage.setItem('deviceId', config.DEVICE_ID);
-            return;
-        }
+    if (config.DEVICE_ID !== null) {
+      localStorage.setItem("deviceId", config.DEVICE_ID);
+      return;
+    }
 
-        if (!localStorage.getItem('deviceId')) {
-            const newDeviceId = v4();
-            localStorage.setItem('deviceId', newDeviceId);
-        }
-    }, [config]);
+    if (!localStorage.getItem("deviceId")) {
+      const newDeviceId = v4();
+      localStorage.setItem("deviceId", newDeviceId);
+    }
+  }, [config]);
 
-    useEffect(() => {
-        if (config){
-            AuthService.checkAuthentication(config).then(isAuthenticated => setIsAuthenticated(isAuthenticated));
+  useEffect(() => {
+    if (config) {
+      AuthService.checkAuthentication(config).then((isAuthenticated) =>
+        setIsAuthenticated(isAuthenticated)
+      );
 
-            if (!isAuthenticated){
-                AuthService.refreshAccessToken(config).then(r => r);
+      if (!isAuthenticated) {
+        AuthService.refreshAccessToken(config).then((r) => r);
+      }
+    }
+  }, [config]);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Root />}>
+            <Route path="" element={<Home />} />
+            <Route path="cart" element={<CartOverview />} />
+            <Route path="table/:id" element={<ScannedTable />} />
+          </Route>
+
+          <Route path="/order" element={<Root />}>
+            <Route path="progress/:orderId" element={<OrderProgress />} />
+          </Route>
+
+          {/*AUTH*/}
+          <Route
+            path="/login"
+            element={<Login setIsAuthenticated={setIsAuthenticated} />}
+          />
+
+          {/*ADMIN*/}
+          <Route
+            path={"/admin"}
+            element={
+              isAuthenticated ? (
+                <AdminRoot setIsAuthenticated={setIsAuthenticated} />
+              ) : (
+                <Navigate to="/login" />
+              )
             }
-        }
-    }, [config]);
+          >
+            <Route path={""} element={<Dashboard />} />
+            <Route path={"tables"} element={<Tables />} />
+            <Route path={"tables/create"} element={<TablesCreate />} />
+            <Route path={"tables/:id/edit"} element={<TablesEdit />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
 
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Root/>}>
-                        <Route path="" element={<Home />} />
-                        <Route path="cart" element={<CartOverview />} />
-                        <Route path="table/:id" element={<ScannedTable />} />
-                    </Route>
-
-                    <Route path="/order" element={<Root/>}>
-                        <Route path="progress" element={<OrderProgress />} />
-                    </Route>
-
-                    {/*AUTH*/}
-                    <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} /> } />
-
-                    {/*ADMIN*/}
-                    <Route path={"/admin"} element={isAuthenticated ? <AdminRoot setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />}>
-                        <Route path={""} element={<Dashboard />}/>
-                        <Route path={"tables"} element={<Tables />} />
-                        <Route path={"tables/create"} element={<TablesCreate />} />
-                        <Route path={"tables/:id/edit"} element={<TablesEdit />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-
-            <ToastContainer stacked />
-        </>
-    )
+      <ToastContainer stacked />
+    </>
+  );
 }
 
-export default App
+export default App;
