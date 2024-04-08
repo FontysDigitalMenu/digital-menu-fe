4d1010f40995cbfd59cd4128634e8097113caefa
Allertvdw <allertvdw@hotmail.com>
2024-04-08 13:46:41 +0200

    Added get method for orders (bugged)

--- src/components/elements/MultipleContainers.jsx
+++ src/components/elements/MultipleContainers.jsx
@@ -1,10 +1,23 @@
 import PlusIcon from "../icons/PlusIcon";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import ColumnContainer from "./ColumnContainer";
-import { DndContext, DragOverlay, KeyboardSensor, MouseSensor, TouchSensor, useSensor, useSensors} from "@dnd-kit/core";
-import {SortableContext, arrayMove, sortableKeyboardCoordinates} from "@dnd-kit/sortable";
+import {
+  DndContext,
+  DragOverlay,
+  KeyboardSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  SortableContext,
+  arrayMove,
+  sortableKeyboardCoordinates,
+} from "@dnd-kit/sortable";
 import { createPortal } from "react-dom";
 import TaskCard from "./SortableItem";
+import ToastNotification from "../notifications/ToastNotification";
 
 const defaultCols = [
   {
@@ -45,28 +58,55 @@ function MultipleContainers() {
   const [tasks, setTasks] = useState(defaultTasks);
   const [activeColumn, setActiveColumn] = useState(null);
   const [activeTask, setActiveTask] = useState(null);
+  const [order, setOrder] = useState([]);
 
   const sensors = useSensors(
-      useSensor(MouseSensor, {
-        activationConstraint: {
-          distance: 10,
-        },
-      }),
-      useSensor(TouchSensor, {
-        activationConstraint: {
-          delay: 75,
-          tolerance: 5,
-        },
-      }),
-      useSensor(KeyboardSensor, {
-        coordinateGetter: sortableKeyboardCoordinates,
-      })
+    useSensor(MouseSensor, {
+      activationConstraint: {
+        distance: 10,
+      },
+    }),
+    useSensor(TouchSensor, {
+      activationConstraint: {
+        delay: 75,
+        tolerance: 5,
+      },
+    }),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
   );
 
+  useEffect(() => {
+    fetchOrders().then((r) => r);
+  }, []);
+
+  const fetchOrders = async () => {
+    try {
+      const response = await fetch(`${config.API_URL}/api/v1/order/paid`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Bearer " + localStorage.getItem("accessToken"),
+        },
+      });
+
+      if (!response.ok) {
+        ToastNotification("error", "Failed to fetch orders");
+        return;
+      }
+
+      const data = await response.json();
+      console.log(data);
+      setOrder(data);
+    } catch {
+      ToastNotification("error", "Failed to fetch orders");
+    }
+  };
 
   return (
-    <div
-      className="flex w-full items-center overflow-x-auto overflow-y-hidden pt-10">
+    <div className="flex w-full items-center overflow-x-auto overflow-y-hidden pt-10">
       <DndContext
         sensors={sensors}
         onDragStart={onDragStart}
@@ -75,10 +115,17 @@ function MultipleContainers() {
       >
         <div className="mx-auto flex gap-4 w-full px-2">
           <div className="flex gap-4 w-full">
-            <SortableContext items={columnsId} className="cursor-default w-full">
+            <SortableContext
+              items={columnsId}
+              className="cursor-default w-full"
+            >
               {columns.map((col) => (
-                <ColumnContainer key={col.id} column={col}
-                  deleteTask={deleteTask} updateTask={updateTask} tasks={tasks.filter((task) => task.columnId === col.id)}
+                <ColumnContainer
+                  key={col.id}
+                  column={col}
+                  deleteTask={deleteTask}
+                  updateTask={updateTask}
+                  tasks={tasks.filter((task) => task.columnId === col.id)}
                 />
               ))}
             </SortableContext>
@@ -121,34 +168,34 @@ function MultipleContainers() {
       event.cancel();
       return;
     }
-  
+
     setActiveTask(event.active.data.current.task);
   }
 
   function onDragEnd(event) {
     setActiveTask(null);
-  
+
     const { active, over } = event;
     if (!over) return;
-  
+
     const activeId = active.id;
     const overId = over.id;
-  
+
     const isActiveATask = active.data.current?.type === "Task";
     const isOverATask = over.data.current?.type === "Task";
     const isOverAColumn = over.data.current?.type === "Column";
-  
+
     if (!isActiveATask || !isOverATask) return;
-  
+
     setTasks((tasks) => {
       const activeIndex = tasks.findIndex((t) => t.id === activeId);
       const overIndex = tasks.findIndex((t) => t.id === overId);
-  
+
       if (tasks[activeIndex].columnId !== tasks[overIndex].columnId) {
         tasks[activeIndex].columnId = tasks[overIndex].columnId;
         return arrayMove(tasks, activeIndex, overIndex - 1);
       }
-  
+
       return arrayMove(tasks, activeIndex, overIndex);
     });
   }
