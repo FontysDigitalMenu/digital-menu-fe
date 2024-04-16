c1440c2f428772d45dcfa93e0e34de4ac2c06712
jens <jensramakers@gmail.com>
2024-04-16 18:48:31 +0200

    added websocket with animation

--- src/components/order/OrderProgress.jsx
+++ src/components/order/OrderProgress.jsx
@@ -2,6 +2,8 @@ import { useContext, useEffect, useState } from 'react'
 import ConfigContext from '../../provider/ConfigProvider.jsx'
 import waiter from '../../assets/waiter.jpg'
 import { Link, useParams } from 'react-router-dom'
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
+import notification from '../../assets/progress-notification.mp3'
 
 function OrderProgress() {
     const config = useContext(ConfigContext)
@@ -10,51 +12,90 @@ function OrderProgress() {
     const [loading, setLoading] = useState(true)
     const [paymentStatusText, setPaymentStatusText] = useState('')
     const [waiterPosition, setWaiterPosition] = useState('')
+    const [processingClass, setProcessingClass] = useState('')
+    const [completedClass, setCompletedClass] = useState('')
+
+    const [connection, setConnection] = useState()
 
     useEffect(() => {
         if (!config) return
-        fetchOrder(orderId).then((r) => r)
+
+        const newConnection = new HubConnectionBuilder().withUrl(`${config.API_URL}/api/orderHub`, {}).configureLogging(LogLevel.Critical).withAutomaticReconnect().build()
+        setConnection(newConnection)
     }, [config])
+
+    useEffect(() => {
+        if (!connection) return
+
+        startConnectionAndFetchOrder()
+
+        return () => {
+            if (!connection) return
+
+            connection
+                .stop()
+                .then(() => {
+                    console.log('Connection stopped')
+                })
+                .catch((error) => {
+                    console.log('Connection stopped Error: ' + error)
+                })
+        }
+    }, [connection])
+
+    async function startConnectionAndFetchOrder() {
+        await connection.start()
+
+        connection.on('ReceiveOrderUpdate', (order) => {
+            setOrder(order)
+            const audio = new Audio(notification)
+            audio.play()
+        })
+
+        fetchOrder(orderId).then((r) => r)
+    }
 
     useEffect(() => {
         if (order === undefined || order === null) return
 
         switch (order.status) {
             case 'Pending':
-                setWaiterPosition('justify-start')
+                setWaiterPosition('left-0')
+                setProcessingClass('w-0')
+                setCompletedClass('w-0')
                 break
             case 'Processing':
-                setWaiterPosition('justify-center')
+                setWaiterPosition('left-1/2 -translate-x-1/2')
+                setProcessingClass('w-2/3')
+                setCompletedClass('w-2/3')
                 break
             case 'Completed':
-                setWaiterPosition('justify-end')
+                setWaiterPosition('left-full -translate-x-full')
+                setProcessingClass('w-2/3')
+                setCompletedClass('w-full')
                 break
         }
-
-        let tempPaymentStatusText = ''
 
         switch (order.paymentStatus) {
             case 'Pending':
-                tempPaymentStatusText = 'Processing payment'
+                setPaymentStatusText('Processing payment')
                 break
             case 'Paid':
-                tempPaymentStatusText = 'Thank you for your order!'
+                setPaymentStatusText('Thank you for your order!')
                 break
             case 'Canceled':
-                tempPaymentStatusText = 'Payment canceled'
+                setPaymentStatusText('Payment canceled')
                 break
             case 'Refund':
-                tempPaymentStatusText = 'Payment refunded'
+                setPaymentStatusText('Payment refunded')
                 break
             case 'Expired':
-                tempPaymentStatusText = 'Payment expired'
+                setPaymentStatusText('Payment expired')
                 break
             default:
-                tempPaymentStatusText = 'Unknown payment error'
+                setPaymentStatusText('Unknown payment error')
                 break
         }
-
-        setPaymentStatusText(tempPaymentStatusText)
     }, [order])
 
     async function fetchOrder(orderId) {
@@ -67,7 +108,11 @@ function OrderProgress() {
         })
 
         if (response.status === 200) {
-            setOrder(await response.json())
+            const data = await response.json()
+            setOrder(data)
+
+            const groupName = `order-${data.id}`
+            await connection.invoke('AddToOrderGroup', { groupName })
         } else if (response.status === 404) {
             setOrder(null)
         }
@@ -84,8 +129,8 @@ function OrderProgress() {
                             <div className="title-box text-6xl font-bold w-full px-2 mb-6">
                                 <p className="text-center">{paymentStatusText}</p>
                             </div>
-                            <div className={`flex ${waiterPosition}`}>
-                                <img className="w-20" src={waiter} alt="" />
+                            <div className={`relative h-20`}>
+                                <img className={`w-20 absolute ${waiterPosition} duration-1000 transition-all`} src={waiter} alt="waiter" />
                             </div>
 
                             <div className={'relative text-white'}>
@@ -99,24 +144,21 @@ function OrderProgress() {
                                       "
                                     ></div>
                                 </div>
-                                {(order.status === 'Processing' || order.status === 'Completed') && (
-                                    <div className="flex absolute w-full z-20">
-                                        <div className="rounded-l-lg h-[40px] w-2/3 bg-green-600 flex justify-end items-center pr-3">Processing</div>
-                                        <div
-                                            className="w-0 h-0
+
+                                <div className="flex absolute w-full z-20">
+                                    <div className={`rounded-l-lg h-[40px] ${processingClass} transition-all duration-1000 bg-green-600 flex justify-end items-center pr-3`}>Processing</div>
+                                    <div
+                                        className="w-0 h-0
                                           border-t-[20px] border-t-transparent
                                           border-b-[20px] border-b-transparent
                                           border-l-[30px] border-l-green-600
                                           "
-                                        ></div>
-                                    </div>
-                                )}
+                                    ></div>
+                                </div>
 
-                                {order.status === 'Completed' && (
-                                    <div className="flex absolute w-full z-10">
-                                        <div className="rounded-l-lg h-[40px] w-full bg-green-700 flex justify-end items-center rounded pr-3">Done</div>
-                                    </div>
-                                )}
+                                <div className="flex absolute w-full z-10">
+                                    <div className={`rounded-l-lg h-[40px] ${completedClass} transition-all duration-1000 bg-green-700 flex justify-end items-center rounded pr-3`}>Done</div>
+                                </div>
                             </div>
 
                             <div className="total-box text-2xl font-bold w-full px-2 mt-8 mb-4 pt-4">
