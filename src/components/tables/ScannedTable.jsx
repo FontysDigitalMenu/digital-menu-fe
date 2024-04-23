aba57939c7024cf5656288beea75af9e241531f7
Hu,Tony T.C. <tony.hu@student.fontys.nl>
2024-04-23 15:04:06 +0200

    added addhost to scannedTable

--- src/components/tables/ScannedTable.jsx
+++ src/components/tables/ScannedTable.jsx
@@ -1,14 +1,37 @@
 import { useNavigate, useParams } from 'react-router-dom'
-import { useEffect } from 'react'
+import {useContext, useEffect} from 'react'
+import ConfigContext from "../../provider/ConfigProvider.jsx";
 
 function ScannedTable() {
     const { id } = useParams()
     const navigate = useNavigate()
+    const config = useContext(ConfigContext)
 
     useEffect(() => {
         localStorage.setItem('tableId', id)
-        return navigate('/')
-    }, [])
+
+        if (config) {
+            AddHost().then(() => {
+                return navigate('/')
+            })
+        }
+    }, [config])
+
+    async function AddHost() {
+        const response = await fetch(`${config.API_URL}/api/v1/table/AddHost`, {
+            method: 'POST',
+            body:  new URLSearchParams({
+                id: id,
+                deviceId: localStorage.getItem('deviceId'),
+            })
+        })
+
+        if (response.status === 204) {
+            console.log("successfully added host")
+        } else {
+            console.error("failed set user to host")
+        }
+    }
 }
 
 export default ScannedTable
