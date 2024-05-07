e6837343a1cb77b687a86189b90d4cc37e12edc5
Hu,Tony T.C. <tony.hu@student.fontys.nl>
2024-05-07 12:41:52 +0200

    added modal and changed method name on resetSerssion page

--- src/components/kitchen/WaiterTables.jsx
+++ src/components/kitchen/WaiterTables.jsx
@@ -3,18 +3,32 @@ import ConfigContext from "../../provider/ConfigProvider.jsx";
 import {Link} from "react-router-dom";
 import notification from "react-notifications/lib/Notification.js";
 import ToastNotification from "../notifications/ToastNotification.jsx";
+import IngredientsDelete from "../admin/ingredients/IngredientsDelete.jsx";
+"use client";
+import { Button, Modal } from "flowbite-react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 function WaiterTables() {
-
+    const [modalIsOpen, setIsOpen] = useState(false)
+    const [tableId, setTableId] = useState(0)
+    const [closeCount, setCloseCount] = useState(0)
+    function openModal(id) {
+        setTableId(id)
+        setIsOpen(true)
+    }
+    function closeModal() {
+        setIsOpen(false)
+        setCloseCount((prev) => prev + 1)
+    }
     const config = useContext(ConfigContext)
     const [tables, setTables] = useState([])
 
     useEffect(() => {
         if (!config) return
-        fetchQrCode().then((r) => r)
+        fetchTables().then((r) => r)
     }, [config])
 
-    async function fetchQrCode() {
+    async function fetchTables() {
         const response = await fetch(`${config.API_URL}/api/v1/Table`, {
             method: 'GET',
             headers: {
@@ -29,7 +43,8 @@ function WaiterTables() {
         setTables(data)
     }
 
-    function resetSession(tableId){
+    function resetSession(){
+        closeModal();
         fetch(`${config.API_URL}/api/v1/Table/ResetSession/${tableId}`, {
             method: 'POST',
             headers: {
@@ -54,11 +69,30 @@ function WaiterTables() {
 
     return (
         <div>
+            <Modal show={modalIsOpen} size="md" onClose={() => closeModal()} popup>
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Are you sure you want to reset this table?
+                        </h3>
+                        <div className="flex justify-center gap-4">
+                            <Button color="failure" onClick={() => resetSession()}>
+                                {"Yes, I'm sure"}
+                            </Button>
+                            <Button color="gray" onClick={() => closeModal()}>
+                                No, cancel
+                            </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
             {tables.map((table, index) => (
                 <div key={index} className="border border-gray-500 px-2 py-1 rounded w-36 m-1">
                     <div className="flex justify-between ">
-                        <div className="mr-4">{table.name}</div>
-                        <button onClick={() => resetSession(table.id)} className="flex items-center justify-end">
+                    <div className="mr-4">{table.name}</div>
+                        <button onClick={() => openModal(table.id)} className="flex items-center justify-end">
                             <span className="material-symbols-outlined text-red-500">
                                 history
                             </span>
@@ -67,7 +101,6 @@ function WaiterTables() {
                 </div>
             ))}
         </div>
-
     );
 }
 
